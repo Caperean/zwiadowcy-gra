d65f073ec7b62e93946297c14045e854a4be05f0
szczurek-cyber <piotr.pionnier@gmail.com>
2025-09-11 20:44:20 +0200

    Update level1.js

--- levels/level1.js
+++ levels/level1.js
@@ -18,18 +18,7 @@ export const level1 = {
         ".........................",
     ],
     objects: [
-        { type: "player", x: 50, y: 50 },
-        { type: "fire", x: 100, y: 416 }, // Dodany obiekt ognia
-        { type: "wolf", x: 500, y: 416 },//dodany wilk
        
-        { type: "bush", x: 400, y: 420, width: 64, height: 40 },
-        {type: "wolf",  x: 400, y:416},
-        { type: "exitGate", x: 700, y: 330, width: 80, height: 130 },
-        {type: "snowytree",x: 50, y:50,width: 80, height:130 },
-        {type: "wolf", x: 50, y: 416 },
-        {type: "fire", x: 150, y: 100},
-        {type: "hill",x: 50, y:50,width: 80, height:130 }
-      
         
     ]
 };
