ca648e624a125a24c411a3fbe0aad522bfb48f59
szczurek-cyber <piotr.pionnier@gmail.com>
2025-09-11 20:14:07 +0200

    Update level1.js

--- levels/level1.js
+++ levels/level1.js
@@ -1,7 +1,7 @@
 export const level1 = {
     backgroundColor: "#add8e6",
     map: [
-        "RRRRRRRRRRRRRWRRRRRRRRRRR",                            // R-skała
+        "RRRR.........WRRRRRRRRRRR",                            // R-skała
         "R............W..........R",                             // G-trawa
         "R............W..........R",                             // #-blok ziemi
         "R............WW.........R",                             // N-śnieg
@@ -21,14 +21,14 @@ export const level1 = {
         { type: "player", x: 50, y: 50 },
         { type: "fire", x: 100, y: 416 }, // Dodany obiekt ognia
         { type: "wolf", x: 500, y: 416 },//dodany wilk
-        { type: "tree", x: 350, y: 100, width: 156, height: 200 },
+       
         { type: "bush", x: 400, y: 420, width: 64, height: 40 },
         {type: "wolf",  x: 400, y:416},
         { type: "exitGate", x: 700, y: 330, width: 80, height: 130 },
         {type: "snowytree",x: 50, y:50,width: 80, height:130 },
         {type: "wolf", x: 50, y: 416 },
         {type: "fire", x: 150, y: 100},
-        {type: "volcano", x: 140, y: 100, width: 156, height:200 },
+      
         
     ]
 };
