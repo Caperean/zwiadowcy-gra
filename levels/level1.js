c0df5f485ba2fe0f977af8b978d32277a40567d3
szczurek-cyber <piotr.pionnier@gmail.com>
2025-09-12 20:05:25 +0200

    Update level1.js

--- levels/level1.js
+++ levels/level1.js
@@ -19,7 +19,7 @@ export const level1 = {
     ],
     objects: [
        { type: "player", x: 15, y: 15 },
-       { type: "wolf", x: 100, y: 15 }       
+       { type: "wolf", x: 100, y: 15 } ,      
        { type: "wolf", x: 700, y: 15 }   
     ]
 };
