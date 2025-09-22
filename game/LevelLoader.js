9d2540e33331497d4af7de3004bf2f600075e5e5
Caperean <sec.guild.deeeep@gmail.com>
2025-09-22 17:20:42 +0200

    Update LevelLoader.js

--- game/LevelLoader.js
+++ game/LevelLoader.js
@@ -109,7 +109,7 @@ export class LevelLoader {
                 const bluePot = new Potion(objData.x, objData.y, "blue");
                 gameObjects.push(bluePot);
             }  
-            } else if (objData.type === "mage") { // <--- Dodaj logikę dla maga
+            } else if (objData.type === "mage") {
                 const mage = new Mage(objData.x, objData.y, game);
                 gameObjects.push(mage);
             }
