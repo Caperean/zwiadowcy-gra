ed2569c480badbe26b4faca0b4e6c6db4fde9555
Caperean <sec.guild.deeeep@gmail.com>
2025-09-22 16:13:38 +0200

    Create levels.js

---
+++ levels/levels.js
@@ -0,0 +1,9 @@
+import { level1 } from "./level1.js";
+
+// Tutaj będziesz dodawać kolejne poziomy
+export const allLevels = [
+    { name: "level1.js", data: level1 },
+    // Dodaj tu więcej poziomów, np.:
+    // { name: "level2.js", data: level2 },
+    // { name: "level3.js", data: level3 },
+];
