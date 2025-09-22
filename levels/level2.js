5f69f8ac95ae61dc029088445339137708d547eb
Caperean <sec.guild.deeeep@gmail.com>
2025-09-22 16:21:48 +0200

    Update level2.js

--- levels/level2.js
+++ levels/level2.js
@@ -20,6 +20,7 @@ export const level2 = {
     objects: [
        { type: "player", x: 15, y: 15 },
        { type: "wolf", x: 100, y: 15 } ,      
-       { type: "wolf", x: 400, y: 100 }   
+       { type: "wolf", x: 400, y: 100 },
+       { type: "exitgate", x: 400, y: 100 }
     ]
 };
