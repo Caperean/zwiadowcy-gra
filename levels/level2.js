7f048b39583ce379faca5e0fae79e3d2f0b5e5c2
szczurek-cyber <piotr.pionnier@gmail.com>
2025-09-23 10:55:36 +0200

    Update level2.js

--- levels/level2.js
+++ levels/level2.js
@@ -19,7 +19,7 @@ export const level2 = {
     ],
     objects: [
        { type: "player", x: 15, y: 15 },
-       { type: "Mage", x: 600, y: 300 }, 
+       { type: "Mage", x: 700, y: 400 }, 
    
        
     ]
