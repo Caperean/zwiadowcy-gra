8bdb189765bf4afb3250394f81618665af5c10dd
Caperean <sec.guild.deeeep@gmail.com>
2025-09-24 21:45:06 +0200

    Update level1.js

--- levels/level1.js
+++ levels/level1.js
@@ -20,7 +20,7 @@ export const level1 = {
     objects: [
        { type: "player", x: 15, y: 15 },
        { type: "wolf", x: 100, y: 15 } ,      
-       { type: "wolf", x: 400, y: 100 },
+       { type: "arab", x: 400, y: 100 },
        
         { type: "exitGate", x: 700, y: 400, width: 64, height: 64 }
     ]
