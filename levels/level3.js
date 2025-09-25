1c3680439b8a0ceeb14793b90bf33f8d843521bb
Caperean <sec.guild.deeeep@gmail.com>
2025-09-25 18:03:31 +0200

    Update level3.js

--- levels/level3.js
+++ levels/level3.js
@@ -20,7 +20,8 @@ export const level3 = {
     objects: [
        { type: "player", x: 50, y: 15 },
        { type: "cactus", x: 100, y: 100, width: 140, height: 200 },
-        { type: "bat", x: 500, y: 400 }
+        { type: "bat", x: 500, y: 400 },
+        { type: "arab", x: 400, y: 100 },
       
        
     ]
