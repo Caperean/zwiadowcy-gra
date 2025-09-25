e1c2cf51bb298fa382498a7e2dd2acaecdf901b0
Caperean <sec.guild.deeeep@gmail.com>
2025-09-25 17:15:32 +0200

    Update EnemyArrow.js

--- objects/EnemyArrow.js
+++ objects/EnemyArrow.js
@@ -7,9 +7,15 @@ export class EnemyArrow extends GameObject {
         super(x, y, ARROW_WIDTH, ARROW_HEIGHT);
         this.game = game;
         this.speed = ARROW_SPEED;
-        this.dx = dx * this.speed;
-        this.dy = dy * this.speed;
+        
+        // Oblicz prędkość w oparciu o znormalizowane wektory
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        this.dx = (dx / distance) * this.speed;
+        this.dy = (dy / distance) * this.speed;
+        
         this.toRemove = false;
+        
+        // Zapisz kąt lotu dla rysowania
         this.flightAngle = Math.atan2(this.dy, this.dx);
 
         this.sprite = new Image();
@@ -20,14 +26,12 @@ export class EnemyArrow extends GameObject {
         this.x += this.dx;
         this.y += this.dy;
 
-        // Kolizja z graczem
         if (this.checkCollision(this.game.player)) {
             this.game.player.takeDamage(1);
             this.toRemove = true;
             return;
         }
 
-        // Kolizja z kafelkami
         this.game.gameObjects.forEach(obj => {
             if (obj instanceof Tile && this.checkCollision(obj)) {
                 this.toRemove = true;
