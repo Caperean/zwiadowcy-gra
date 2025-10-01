1ee8d5c64e3fcdf087282c34a4468c121677b606
Caperean <sec.guild.deeeep@gmail.com>
2025-10-01 19:54:48 +0200

    Update arrow.js

--- objects/arrow.js
+++ objects/arrow.js
@@ -2,12 +2,15 @@ import { GameObject } from "./object.js";
 import { ARROW_WIDTH, ARROW_HEIGHT } from "../engine/Constants.js";
 import { Tile } from "./tile.js";
 import { Fire } from "./fire.js";
-import { Wolf } from "./wolf.js"; // ZMIANA: Dodano importy przeciwników
-import { Mage } from "./Mage.js";
-import { Bat } from "./Bat.js";
-import { Arab } from "./Arab.js";
 
 export class Arrow extends GameObject {
+    /**
+     * @param {number} x - Pozycja X startowa strzały.
+     * @param {number} y - Pozycja Y startowa strzały.
+     * @param {number} dx - Prędkość w poziomie.
+     * @param {number} dy - Prędkość w pionie.
+     * @param {object} game - Obiekt gry, potrzebny do kolizji z kafelkami.
+     */
     constructor(x, y, dx, dy, game) {
         super(x, y, ARROW_WIDTH, ARROW_HEIGHT);
         this.dx = dx;
@@ -18,33 +21,36 @@ export class Arrow extends GameObject {
         this.burningSprite = new Image();
         this.burningSprite.src = "assets/sprites/burningArrow.png";
         this.isFired = true;
-        this.toRemove = false; 
-        this.isBurning = false; 
+        this.toRemove = false; // Flaga do usunięcia strzały
+        this.isBurning = false; // Nowa flaga dla płonącej strzały
     }
     
-    update(deltaTime) {
-        // Sprawdzenie, czy strzała wyleciała poza ekran
-        if (this.x < 0 || this.x > this.game.canvas.width || this.y < 0 || this.y > this.game.canvas.height) {
-            this.toRemove = true;
-        }
+    /**
+     * Sprawdza kolizję z innym obiektem.
+     * @param {GameObject} other - Inny obiekt do sprawdzenia kolizji.
+     * @returns {boolean} - True, jeśli jest kolizja, w przeciwnym razie false.
+     */
+    checkCollision(other) {
+        return this.x < other.x + other.width &&
+               this.x + this.width > other.x &&
+               this.y < other.y + other.height &&
+               this.y + this.height > other.y;
+    }
 
+    /**
+     * Aktualizuje stan strzały w każdej klatce gry.
+     * @param {number} deltaTime - Czas od ostatniej klatki w milisekundach.
+     */
+    update(deltaTime) {
+        if (!this.isFired) return;
+        
         // Sprawdzanie kolizji z kafelkami i ogniem
         this.game.gameObjects.forEach(obj => {
             if (obj instanceof Tile && this.checkCollision(obj)) {
                 this.isFired = false;
-                this.toRemove = true;
+                this.toRemove = true; // Strzała znika po kolizji
             } else if (obj instanceof Fire && this.checkCollision(obj)) {
                 this.isBurning = true;
-            }
-        });
-
-        // ZMIANA: Sprawdzanie kolizji z przeciwnikami
-        this.game.gameObjects.forEach(obj => {
-            if (this.checkCollision(obj)) {
-                if (obj instanceof Wolf || obj instanceof Mage || obj instanceof Bat || obj instanceof Arab) {
-                    obj.takeDamage(1); // Zadaj 1 punkt obrażeń
-                    this.toRemove = true; // Strzała znika po trafieniu
-                }
             }
         });
 
@@ -52,20 +58,31 @@ export class Arrow extends GameObject {
         this.y += this.dy;
     }
 
+    /**
+     * Rysuje strzałę na canvasie.
+     * @param {CanvasRenderingContext2D} ctx - Kontekst rysowania 2D.
+     */
     draw(ctx) {
         const spriteToDraw = this.isBurning ? this.burningSprite : this.sprite;
 
         if (spriteToDraw.complete) {
             ctx.save();
             ctx.translate(this.x + this.width / 2, this.y + this.height / 2);
+
+            // Odwróć strzałę, jeśli leci w lewo
             if (this.dx < 0) {
                 ctx.scale(-1, 1);
             }
+            
+            // Ponieważ sprite jest domyślnie pionowy, obracamy go o 90 stopni
             ctx.rotate(Math.PI / 2);
+            
+            // Rysujemy strzałę z przesunięciem, aby obrót był wokół jej środka
             ctx.drawImage(spriteToDraw, -this.width / 2, -this.height / 2, this.width, this.height);
+            
             ctx.restore();
         } else {
-            ctx.fillStyle = this.isBurning ? "orange" : "white";
+            ctx.fillStyle = this.isBurning ? "orange" : "brown";
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
