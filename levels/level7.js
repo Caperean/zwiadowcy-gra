02d694e820cbd1983cf11b3591bc198e46bd5ae1
Caperean <sec.guild.deeeep@gmail.com>
2025-10-06 17:01:28 +0200

    Update level7.js

--- levels/level7.js
+++ levels/level7.js
@@ -1,12 +1,12 @@
 export const level7 = {
     backgroundColor: "#add8e6",
     map: [
-        ".........................R",                            // R-skała
+        "#........................R",                            // R-skała
         "#...............G........R",                             // G-trawa
         "#GGGGGGGGGGGGGGG#GG......R",                             // #-blok ziemi
-        "#...................G....R",                             // N-śnieg
+        "#..................G....R",                             // N-śnieg
         "#........................R",                            // I-lód
-        "#...GGGG.............GGGR",                            // L-lawa
+        "#...GGGG..............GGGR",                            // L-lawa
         "#..G....GGGGG..GG..GG....R",                           //  W-water 
         "#........................R",                          //   S-kolce
         "#G.......................R",                           //  M-magma
@@ -20,6 +20,6 @@ export const level7 = {
     objects: [
        { type: "player", x: 36, y: 390 },  
         { type: "wolf", x: 40, y: 25 },
-        { type: "exitGate", x: 25, y: 25, width: 64, height: 64 }
+        { type: "exitGate", x: 25, y: 15, width: 64, height: 64 }
     ]
 };
