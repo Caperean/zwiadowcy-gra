5a01047bd4ab1e6ba4decff9257e1f1157ef0529
Caperean <sec.guild.deeeep@gmail.com>
2025-10-07 15:24:46 +0200

    Update PoisonedArrow.js

--- objects/PoisonedArrow.js
+++ objects/PoisonedArrow.js
@@ -1,15 +1,16 @@
+// objects/PoisonedArrow.js
+
 import { Arrow } from "./arrow.js";
 
 export class PoisonedArrow extends Arrow {
     /**
      * @param {number} x - Pozycja X startowa zatrutej strzały.
      * @param {number} y - Pozycja Y startowa zatrutej strzały.
-     * @param {number} dx - Prędkość w poziomie.
-     * @param {number} dy - Prędkość w pionie.
+     * @param {number} powerCharge - Naładowana siła strzału.
      * @param {object} game - Obiekt gry.
      */
-    constructor(x, y, dx, dy, game) {
-        super(x, y, dx, dy, game);
+    constructor(x, y, powerCharge, game) { // Zmienione parametry!
+        super(x, y, powerCharge, game); // Przekazanie powerCharge do bazowego konstruktora
         this.sprite.src = "assets/sprites/poisonedarrow.png"; // Zmieniamy sprite
     }
 }
