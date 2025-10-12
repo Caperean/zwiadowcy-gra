eb61d70b6275a8bd686fb196d39afec0b401bb86
Caperean <sec.guild.deeeep@gmail.com>
2025-10-12 16:50:32 +0200

    Update level0.js

--- levels/level0.js
+++ levels/level0.js
@@ -6,15 +6,15 @@ export const level0 = {                             //poziom testowy żeby nie p
         "....C.............PPSPSP.#",                             // #-blok ziemi
         "..C..................PP..#",                             // N-śnieg
         "..CC.......PP............#",                            // I-lód
-        "....C...P....PPP.P......#",                            // L-lawa
-        ".CC.....S.......PSP......#",                              // P- piasek
-        ".CCC......PP.......P.PP.P#",                            //   S-kolce
+        "....C...P....PPP..P......#",                            // L-lawa
+        ".CC.....P.......PSP......#",                              // P- piasek
+        ".CCC.....SPP.......P.PP.P#",                            //   S-kolce
         ".CC.C.......PPP....PSPPSP#",                           //  M-magma
         "C.C.............CCCCC....#",                             // A- marmur
-        "C..C..............C...C..#",                           // C- cegła
-        "CC......................C#",
+        "C..C.....................#",                           // C- cegła
+        "CC.......................#",
         ".........P...............#",
-        "...CP....P....PPPPPPPPPPP#", 
+        "...CP....P....PPP.......P#", 
         "CCCCPPPPPPPPPPPPPPPPPPPPP#",
         ],
     objects: [
@@ -27,6 +27,8 @@ export const level0 = {                             //poziom testowy żeby nie p
         { type: "arab", x: 96, y: 46 },  
          { type: "arab", x: 33, y: 300 },
         { type: "bat", x: 280, y: 96 },
+        { type: "wolf", x: 604, y: 400 },
+         { type: "arab", x: 620, y: 400 },
         { type: "exitGate", x: 46, y: 390, width: 64, height: 64 }
     ]
 };
