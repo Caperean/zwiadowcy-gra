8a8c37ce3dbeac339152a0cfd3fbf133069f552f
Caperean <sec.guild.deeeep@gmail.com>
2025-10-12 12:17:36 +0200

    Update level0.js

--- levels/level0.js
+++ levels/level0.js
@@ -23,7 +23,7 @@ export const level0 = {                             //poziom testowy żeby nie p
         { type: "arab", x: 96, y: 158 },
         { type: "wolf", x: 320, y: 400 },
         { type: "arab", x: 335, y: 400 },  
-         { type: "wolf", x: 280, y: 400 },      
+         { type: "wolf", x: 240, y: 400 },      
        
         { type: "exitGate", x: 46, y: 390, width: 64, height: 64 }
     ]
