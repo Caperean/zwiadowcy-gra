d36e9307d678a77d48bc6804f9cfb25c39b15fbf
szczurek-cyber <piotr.pionnier@gmail.com>
2025-10-13 08:55:29 +0200

    Update level3.js

--- levels/level3.js
+++ levels/level3.js
@@ -29,6 +29,9 @@ export const level3 = {
           { type: "arab", x: 300, y: 332 },
           { type: "arab", x: 350, y: 332 },
           { type: "arab", x: 400, y: 332 },
+         { type: "arab", x: 500, y: 332 },
+          { type: "arab", x: 450, y: 332 },
+          { type: "arab", x: 550, y: 332 },
          { type: "exitGate", x: 700, y: 400, width: 64, height: 64 }
       
        
