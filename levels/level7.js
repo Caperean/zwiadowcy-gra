c838b6dacaf67d7bbeb024ac48fb3f338f0cb492
Caperean <sec.guild.deeeep@gmail.com>
2025-10-13 22:56:18 +0200

    Update level7.js

--- levels/level7.js
+++ levels/level7.js
@@ -20,9 +20,9 @@ export const level7 = {
     objects: [
        { type: "player", x: 36, y: 390 },  
         { type: "wolf", x: 40, y: 25 },
-         { type: "spruce", x: 36, y: 390, width: 40, height: 85 },
-         { type: "bush", x: 150, y: 390, width: 25, height: 20 },
-         { type: "bush", x: 160, y: 15, width: 25, height: 20 },
+         { type: "spruce", x: 36, y: 380, width: 48, height: 100 },
+         { type: "bush", x: 150, y: 410, width: 60, height: 30 },
+         { type: "bush", x: 160, y: 35, width: 60, height: 30 },
         { type: "exitGate", x: 25, y: 15, width: 64, height: 64 }
     ]
 };
