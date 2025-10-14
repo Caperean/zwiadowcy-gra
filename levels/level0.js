4ea5f54cfdd0f7da9322322f7b943c1a0308a56d
szczurek-cyber <piotr.pionnier@gmail.com>
2025-10-14 10:41:55 +0200

    Update level0.js

--- levels/level0.js
+++ levels/level0.js
@@ -21,7 +21,8 @@ export const level0 = {                             //poziom testowy żeby nie p
     objects: [
          { type: "player", x: 750, y: 332 },
            { type: "oar", x: 400, y: 50 },// nie używać
-         { type: "exitGate", x: 50, y: 100, width: 64, height: 64 }
+         { type: "exitGate", x: 600, y: 350, width: 64, height: 64 },
+         { type: "bat, x: 150, y: 50 },
     ]
        
 };
