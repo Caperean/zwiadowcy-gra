daf65cd2db7694be25f93c0d38b0513d08146507
Caperean <sec.guild.deeeep@gmail.com>
2025-10-16 22:27:18 +0200

    Fix mage Y-coordinate in level66.js

--- levels/level66.js
+++ levels/level66.js
@@ -19,7 +19,7 @@ export const level66 = {
     ],
     objects: [
        { type: "player", x: 420, y: 150 },
-        { type: "mage", x: 710, y: 4 },
+        { type: "mage", x: 710, y: 0 },
         { type: "bat", x: 320, y: 320 },
        
         { type: "exitGate", x: 730, y: 66, width: 64, height: 64 }
