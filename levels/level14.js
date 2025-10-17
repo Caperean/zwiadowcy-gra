76016404ef1897779ab3241a48157bdeb1cca037
Caperean <sec.guild.deeeep@gmail.com>
2025-10-17 23:24:13 +0200

    Update level14.js

--- levels/level14.js
+++ levels/level14.js
@@ -21,7 +21,7 @@ export const level14 = {
        { type: "player", x: 700, y: 33 },
        { type: "boar", x: 500, y: 15 } ,      
          { type: "bush", x: 15, y: 130, width: 100, height: 80 } ,
-         { type: "hill", x: 400, y: -100, width: 400, height: 310 ,
+         { type: "hill", x: 400, y: -100, width: 400, height: 310 } ,
          { type: "house", x: 500, y: 40, width: 200, height: 16 } ,
         
        
