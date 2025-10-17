f2a3db812158f53618a2accf43fd6ddaa81c5651
Caperean <sec.guild.deeeep@gmail.com>
2025-10-17 23:41:55 +0200

    Update level14.js

--- levels/level14.js
+++ levels/level14.js
@@ -23,7 +23,7 @@ export const level14 = {
          { type: "bush", x: 15, y: 130, width: 100, height: 80 } ,
          { type: "hill", x: 410, y: -100, width: 300, height: 300 } ,
          { type: "house", x: 500, y: 60, width: 200, height: 160 } ,
-        { type: "bat", x: 361: 320 } , 
+        { type: "bat", x: 361, y: 320 } , 
        
         { type: "exitGate", x: 700, y: 400, width: 64, height: 64 }
     ]
