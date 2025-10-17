60cd752eaf73d2b84300efc33aa8f8ea0f1e37ab
Caperean <sec.guild.deeeep@gmail.com>
2025-10-17 17:13:22 +0200

    Update level13.js

--- levels/level13.js
+++ levels/level13.js
@@ -34,6 +34,7 @@ export const level13 = {
         { type: "bat", x: 650, y: 20 } ,
          { type: "bat", x: 350, y: 20 } ,
          { type: "bat", x: 400, y: 200 } ,
+         { type: "bat", x: 150, y: 200 } ,
         { type: "exitGate", x: 50, y: 400, width: 64, height: 64 }
     ]
 };
