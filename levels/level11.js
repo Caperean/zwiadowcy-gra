12deaa008e1529875b3f9a6e85bd48bdd4442a98
Caperean <sec.guild.deeeep@gmail.com>
2025-10-17 17:45:03 +0200

    Update level11.js

--- levels/level11.js
+++ levels/level11.js
@@ -1,14 +1,14 @@
 export const level11 = {
     backgroundColor: "#add8e6",
     map: [
-        "S.....RRRRR..............R",                            // R-skała
-        "R...R......R...........R.R",                             // G-trawa
+        "S.....RRRRRR.............R",                            // R-skała
+        "R...R....................R",                             // G-trawa
         "R...........RLRLRLLRLLR..R",                             // #-blok ziemi
         "RR.......R..RRRRRRRRRRR..R",                             // N-śnieg
         "R.....RR....R.........RRRR",                            // I-lód
         "R..RR......RR............R",                            // L-lawa
         "RLLR........R............R",                           //  W-water 
-        "RRR....RLLR.RRRRRLLRLLR..R",                          //   S-kolce
+        "RRR....RRLR.RRRRRLLRLLR..R",                          //   S-kolce
         "M....M.MMM.......MMMMM...R",                           //  M-magma
         "M...MM...................R",                             // A-marmur
         "M..M.....................R",                           // C-cegły
@@ -22,6 +22,6 @@ export const level11 = {
         { type: "mage", x: 700, y: 0 },
         { type: "bat", x: 350, y: 280 },
        
-        { type: "exitGate", x: 710, y: 65, width: 64, height: 64 }
+        { type: "exitGate", x: 730, y: 65, width: 64, height: 64 }
     ]
 };
