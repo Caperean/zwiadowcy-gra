36df093e074eb1549afb96b3d752c45d99a97804
Caperean <sec.guild.deeeep@gmail.com>
2025-10-17 17:00:19 +0200

    Update level13.js

--- levels/level13.js
+++ levels/level13.js
@@ -1,12 +1,12 @@
 export const level13 = {
     backgroundColor: "#add8e6",
     map: [
-        "#........................M",                            // R-skała
+        "S........................M",                            // R-skała
         "#........................M",                             // G-trawa
-        "#........................M",                             // #-blok ziemi
-        "#........................M",                             // N-śnieg
-        "#...................G....M",                            // I-lód
-        "#GGGGGGGGGGGGGGGGGGG#....M",                            // L-lawa
+        "#G.......................M",                             // #-blok ziemi
+        "#...................G....M",                                                       // N-śnieg
+        "#GGGGGGGGGGGGGGGGGGG#....M",                         // I-lód
+        "#........................M",                           // L-lawa
         "#........................M",                           //  W-water 
         "#........................M",                          //   S-kolce
         "#........................P",                           //  M-magma
@@ -14,17 +14,19 @@ export const level13 = {
         "#........................P",                           
         "#........................P",                           
         "#........................P",                           
-        "#....................G..#",
-        "#GGGGGGGGGGGGGGGGGGGG#WWWG",
+        "#........................#",
+        "#GGGGGGGGGGGGGGGGGGGGGGGGG",
     ],
     objects: [
-       { type: "player", x: 20, y: 33 },
+       { type: "player", x: 33, y: 0 },
        { type: "wolf", x: 100, y: 15 } ,      
-         { type: "tree", x: 80, y: 100, width: 120, height: 200 } ,  
+        { type: "boar", x: 270, y: 100 } ,
+         { type: "tree", x: 80, y: 100, width: 180, height: 280 } ,  
        { type: "boar", x: 100, y: 300 } ,
         { type: "wolf", x: 200, y: 15 } ,  
         { type: "wolf", x: 400, y: 300 } ,  
-        { type: "wolf", x: 500, y: 300 } ,  
+        { type: "wolf", x: 580, y: 300 } ,
+        { type: "bat", x: 700, y: 100 } ,
         { type: "exitGate", x: 50, y: 400, width: 64, height: 64 }
     ]
 };
