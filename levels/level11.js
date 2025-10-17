2820a2bc5b5d540b549e19bd2cafd943a11dedcf
Caperean <sec.guild.deeeep@gmail.com>
2025-10-17 17:43:05 +0200

    Update level11.js

--- levels/level11.js
+++ levels/level11.js
@@ -22,6 +22,6 @@ export const level11 = {
         { type: "mage", x: 700, y: 0 },
         { type: "bat", x: 350, y: 280 },
        
-        { type: "exitGate", x: 500, y: 150, width: 64, height: 64 }
+        { type: "exitGate", x: 700, y: 50, width: 64, height: 64 }
     ]
 };
