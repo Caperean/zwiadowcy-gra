f12e2514c9c81185a2ab4c7a8c5ba11304255bf4
Caperean <sec.guild.deeeep@gmail.com>
2025-10-19 16:32:50 +0200

    Create level15.js

---
+++ levels/level15.js
@@ -0,0 +1,27 @@
+export const level15 = {
+    backgroundColor: "#add8e6",
+    map: [
+        "N........................",                            // R-skała
+        "#........................",                             // G-trawa
+        "#....NWIIWIIWIIWNN.......",                             // #-blok ziemi
+        "#NNNN#############.......",                             // N-śnieg
+        "#........................",                            // I-lód
+        "#........................",                            // L-lawa
+        "#........................",                           //  W-water 
+        "#...N...................N",                          //   S-kolce
+        "#...#NNNNNN.NNN.NN.NNNNNN",                           //  M-magma
+        "#........................",                             
+        "#........................",                           
+        "#........................",                           
+        "#.......................N",                           
+        "#......................N#",
+        "#NWIIWWIWIIWIIWIWIIWIIWN#N",
+    ],
+    objects: [
+       { type: "player", x: 20, y: 33 },
+       { type: "wolf", x: 100, y: 15 } ,      
+         { type: "snowytree", x: 800, y: 100, width: 120, height: 80 } ,  
+       
+        { type: "exitGate", x: 700, y: 400, width: 64, height: 64 }
+    ]
+};
