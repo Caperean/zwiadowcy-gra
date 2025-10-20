311402e5e97f92642a1603e894540b15c8f1e81f
Caperean <sec.guild.deeeep@gmail.com>
2025-10-20 16:03:47 +0200

    Update Snowbullet.js

--- objects/Snowbullet.js
+++ objects/Snowbullet.js
@@ -5,22 +5,24 @@ import { GameObject } from "./object.js";
 export class Snowbullet extends GameObject {
     // ... istniejący konstruktor i update ...
 
+   
+    // objects/Snowbullet.js (Poprawiona metoda draw)
+
     draw(ctx) {
         if (this.sprite.complete) {
             ctx.save();
             
+            // Przesuń punkt odniesienia do środka pocisku, aby skalowanie działało poprawnie
+            ctx.translate(this.x + this.width / 2, this.y + this.height / 2);
+            
             // Odbicie w poziomie, jeśli pocisk leci w lewo (dx < 0)
             if (this.dx < 0) {
-                // Przesuń punkt odniesienia do prawej krawędzi pocisku
-                ctx.translate(this.x + this.width, this.y);
-                // Odbij w poziomie
-                ctx.scale(-1, 1);
-                // Rysuj obrazek
-                ctx.drawImage(this.sprite, 0, 0, this.width, this.height);
-            } else {
-                // Rysuj normalnie, jeśli pocisk leci w prawo (dx >= 0)
-                ctx.drawImage(this.sprite, this.x, this.y, this.width, this.height);
+                // Skalowanie na -1 na osi X odwraca sprite
+                ctx.scale(-1, 1); 
             }
+            
+            // Rysuj obrazek od środka nowego układu odniesienia
+            ctx.drawImage(this.sprite, -this.width / 2, -this.height / 2, this.width, this.height);
             
             ctx.restore();
         }
