4aa2d3f42fbd7b9bd9dff0458612d478519e3da6
Caperean <sec.guild.deeeep@gmail.com>
2025-10-20 17:33:16 +0200

    Update Snowman.js

--- objects/Snowman.js
+++ objects/Snowman.js
@@ -1,17 +1,16 @@
 // objects/Snowman.js
 import { GameObject } from "./object.js";
 import { Tile } from "./tile.js";
-import { Snowball } from "./Snowball.js"; // Lekki pocisk
-import { Snowbullet } from "./Snowbullet.js"; // Ciężki pocisk
+import { Snowball } from "./Snowball.js"; 
+import { Snowbullet } from "./Snowbullet.js"; 
+import { Player } from "./player.js"; 
 import { GRAVITY, SNOWMAN_WIDTH, SNOWMAN_HEIGHT, SNOWMAN_HP, SNOWMAN_DETECTION_RANGE, SNOWBALL_COOLDOWN, SNOWBULLET_COOLDOWN } from "../engine/Constants.js";
 
 export class Snowman extends GameObject {
     constructor(x, y, game) {
         super(x, y, SNOWMAN_WIDTH, SNOWMAN_HEIGHT);
         this.game = game;
-        this.initialX = x;
-        this.initialY = y;
-        this.hp = SNOWMAN_HP;
+        this.hp = SNOWMAN_HP; 
         this.dx = 0; // Bałwan się nie porusza
         this.dy = 0;
         this.onGround = false;
@@ -24,12 +23,9 @@ export class Snowman extends GameObject {
         this.sprite.src = "assets/sprites/snowman.png";
     }
 
+    // Metoda do resetowania HP i statusu (przydatna w Game.resetLevelObjects)
     resetPosition() {
-        this.x = this.initialX;
-        this.y = this.initialY;
         this.hp = SNOWMAN_HP;
-        this.dx = 0;
-        this.dy = 0;
         this.toRemove = false;
     }
     
@@ -39,34 +35,30 @@ export class Snowman extends GameObject {
             return;
         }
         
-        // 1. Grawitacja
+        // 1. Zastosowanie grawitacji (Logika Clown.js)
         this.dy += GRAVITY;
         this.y += this.dy;
 
-        // 2. Kolizja z kafelkami
+        // 2. Sprawdzanie kolizji z kafelkami (Logika Clown.js + obsługa IceBlock)
         const oldY = this.y - this.dy;
         this.game.gameObjects.forEach(obj => {
-            if (obj instanceof Tile || (obj.isTile && obj.isTile === true)) {
-                if (this.checkCollision(obj, { x: this.x, y: oldY, width: this.width, height: this.height })) {
-                    // Kolizja z góry
-                    if (this.dy > 0 && this.y + this.height > obj.y) { 
-                        this.y = obj.y - this.height;
-                        this.dy = 0;
-                        this.onGround = true;
-                    } 
-                    // Kolizja z dołu
-                    else if (this.dy < 0 && this.y < obj.y + obj.height) { 
-                        this.y = obj.y + obj.height;
-                        this.dy = 0;
-                    }
+            // Sprawdza Tile lub obiekty działające jak Tile (np. IceBlock, dzięki this.isTile)
+            if ((obj instanceof Tile || (obj.isTile && obj.isTile === true)) && this.checkCollision(obj, {x: this.x, y: oldY, width: this.width, height: this.height})) {
+                if (this.dy > 0) { // Kolizja z góry
+                    this.y = obj.y - this.height;
+                    this.dy = 0;
+                    this.onGround = true;
+                } else if (this.dy < 0) { // Kolizja z dołu
+                    this.y = obj.y + obj.height;
+                    this.dy = 0;
                 }
             }
         });
-
-        // 3. Wykrywanie gracza i Atak
+        
         const player = this.game.player;
         if (!player) return;
 
+        // 3. Wykrywanie gracza i Atak
         const distanceToPlayer = Math.sqrt(Math.pow(player.x - this.x, 2) + Math.pow(player.y - this.y, 2));
 
         if (distanceToPlayer < SNOWMAN_DETECTION_RANGE) {
@@ -86,18 +78,20 @@ export class Snowman extends GameObject {
         }
     }
 
-    // Metoda strzelania Śnieżką
+    // Strzelanie Śnieżką (lekki pocisk)
     shootSnowball(player) {
-        const dx = player.x - this.x;
-        const dy = player.y - this.y;
+        // Obliczenie wektora z centrum Bałwana do centrum Gracza
+        const dx = (player.x + player.width / 2) - (this.x + this.width / 2);
+        const dy = (player.y + player.height / 2) - (this.y + this.height / 2);
         const snowball = new Snowball(this.x + this.width / 2, this.y + this.height / 2, dx, dy, this.game);
         this.game.gameObjects.push(snowball);
     }
     
-    // Metoda strzelania Lodowym Pociskiem
+    // Strzelanie Lodowym Pociskiem (ciężki pocisk)
     shootSnowbullet(player) {
-        const dx = player.x - this.x;
-        const dy = player.y - this.y;
+        // Obliczenie wektora z centrum Bałwana do centrum Gracza
+        const dx = (player.x + player.width / 2) - (this.x + this.width / 2);
+        const dy = (player.y + player.height / 2) - (this.y + this.height / 2);
         const snowbullet = new Snowbullet(this.x + this.width / 2, this.y + this.height / 2, dx, dy, this.game);
         this.game.gameObjects.push(snowbullet);
     }
