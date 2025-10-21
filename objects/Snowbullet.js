f95bd127dc2bdb84bba4c8ff4cc66f041e33144f
Caperean <sec.guild.deeeep@gmail.com>
2025-10-21 23:09:55 +0200

    Update Snowbullet.js

--- objects/Snowbullet.js
+++ objects/Snowbullet.js
@@ -1,1 +1,66 @@
 // objects/Snowbullet.js
+import { GameObject } from "./object.js";
+import { Tile } from "./tile.js";
+import { SB_WIDTH, SB_HEIGHT, SB_SPEED } from "../engine/Constants.js";
+
+
+export class Snowbullet extends GameObject {
+    constructor(x, y, dx, dy, game) {
+        super(x, y, SB_WIDTH, SB_HEIGHT);
+        this.game = game;
+        this.speed = SB_SPEED;
+        
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        this.dx = (dx / distance) * this.speed;
+        this.dy = (dy / distance) * this.speed;
+        
+        this.toRemove = false;
+        
+        // Zapisz kąt lotu dla rysowania
+        this.flightAngle = Math.atan2(this.dy, this.dx);
+        
+        this.sprite = new Image();
+        this.sprite.src = "assets/sprites/snowball.png";
+        this.burningSprite = new Image();
+        ;
+    }
+
+    update(deltaTime) {
+        this.x += this.dx;
+        this.y += this.dy;
+
+        if (this.checkCollision(this.game.player)) {
+            this.game.player.takeDamage(1);
+            this.toRemove = true;
+            return;
+        }
+
+        this.game.gameObjects.forEach(obj => {
+            if (obj instanceof Tile && this.checkCollision(obj)) {
+                this.toRemove = true;
+            
+        });
+    }
+
+    checkCollision(other) {
+        return this.x < other.x + other.width &&
+               this.x + this.width > other.x &&
+               this.y < other.y + other.height &&
+               this.y + this.height > other.y;
+    }
+
+    draw(ctx) {
+        const spriteToDraw = this.isBurning ? this.burningSprite : this.sprite;
+        if (spriteToDraw.complete) {
+            ctx.save();
+            ctx.translate(this.x + this.width / 2, this.y + this.height / 2);
+            // Ta linia poprawnie obraca pionowy sprite strzały, aby leciał prosto
+            ctx.rotate(this.flightAngle + Math.PI / 2);
+            ctx.drawImage(spriteToDraw, -this.width / 2, -this.height / 2, this.width, this.height);
+            ctx.restore();
+        } else {
+            ctx.fillStyle = "gray";
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+}
