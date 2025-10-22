827cac8982fe5aa6a09103c9769ccd99a0412d9a
Caperean <sec.guild.deeeep@gmail.com>
2025-10-22 23:21:49 +0200

    Update level15.js

--- levels/level15.js
+++ levels/level15.js
@@ -25,6 +25,6 @@ export const level15 = {
         { type: "snowman", x: 600, y: 15 } ,
          { type: "wolf", x: 600, y: 15 } ,
          { type: "boar", x: 570, y: 15 } ,
-        { type: "exitGate", x: 630, y: 420, width: 64, height: 64 }
+        { type: "exitGate", x: 655, y: 420, width: 64, height: 64 }
     ]
 };
