e54676580c4554dca969f4538495015bfbb13cfa
Caperean <sec.guild.deeeep@gmail.com>
2025-10-24 23:18:20 +0200

    Update level16.js

--- levels/level16.js
+++ levels/level16.js
@@ -7,12 +7,12 @@ export const level16 = {
         "#..........G...G.........#",                             // N-śnieg
         "#...........GGG..........#",                            // I-lód
         "#........G..........K.K..#",                            // L-lawa
-        "#....G..G..........K.K..#.",                           //  W-water 
+        "#....G..G...........K.K..#.",                           //  W-water 
         "#..G...............KKSK..#.",                          //   S-kolce
         "#.................KKKKK..#",                           //  M-magma
-        "#G................KKKKK..#",                             
-        "#.GG..G..........KKKKKK..#",                           
-        "#.......G........K.......#",                          
+        "#G....G...........KKKKK..#",                             
+        "#................KKKKKK..#",                           
+        "#..G....G........K.......#",                          
         "#.............K.K........#",                           
         "#.....G.......KSK........#",
         "#WWWWWWGGGGGGGKKKKKSKSKSS#", //granica widzenia
@@ -21,7 +21,9 @@ export const level16 = {
     objects: [
        { type: "player", x: 300, y: 330 },
         { type: "boar", x: 360, y: 330 },
-       { type: "clown", x: 660, y: 15 } ,      
+       { type: "clown", x: 660, y: 15 } , 
+        { type: "bat", x: 160, y: 60 } , 
+         { type: "bat", x: 1900, y: 60 } , 
         { type: "clown", x: 685, y: 15 } ,   
         { type: "maj", x: 250, y: 280, width: 170, height: 170 },
         { type: "exitGate", x: 535, y: 400, width: 64, height: 64 }
