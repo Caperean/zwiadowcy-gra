557b79117260584337a2819730891bc030cf910a
Caperean <sec.guild.deeeep@gmail.com>
2025-10-25 11:37:09 +0200

    Update level6.js

--- levels/level6.js
+++ levels/level6.js
@@ -2,20 +2,20 @@ export const level6 = {
     backgroundColor: "#add8e6",
     map: [
         "PPPPPPPPPPPPPPPPPPPPPPPPP",                            // R-skała
-        "P.......................R",                             // G-trawa
-        "P.......................R",                             // #-blok ziemi
-        "AAAAAA.............. ...R",                             // N-śnieg
-        "A.......................R",                            // I-lód
-        "A.......................R",                            // L-lawa
-        "A.......................R",                           //  W-water 
-        "A.......................R",                          //   S-kolce
-        "A.......................R",                           //  M-magma
-        "A.......................R",                             // A-marmur
-        "A....CCCC...............R",                           // C-cegły
-        "A.......C...............R",                           
-        "A.......C...............R",                           
-        "PPPPPPPPPPP.............R",
-        "PPPPPPPPPPPPPPWWWWPPWWWW",
+        "P........................R",                             // G-trawa
+        "P........................R",                             // #-blok ziemi
+        "AAAAAA...................R",                             // N-śnieg
+        "P........................R",                            // I-lód
+        "P........................R",                            // L-lawa
+        "P........................R",                           //  W-water 
+        "P........................R",                          //   S-kolce
+        "P........................R",                           //  M-magma
+        "P........................R",                             // A-marmur
+        "P....CCCC................R",                           // C-cegły
+        "P.......C................R",                           
+        "P.......C................R",                           
+        "PPPPPPPPPPP..............R",
+        "PPPPPPPPPPPPPPWWWWPPWWWWWR",
     ],
     objects: [
        { type: "player", x: 25, y: 25 },
