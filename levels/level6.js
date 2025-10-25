1b458237400d237d7c0577ab5b984ee91256414f
Caperean <sec.guild.deeeep@gmail.com>
2025-10-25 11:58:38 +0200

    Update level6.js

--- levels/level6.js
+++ levels/level6.js
@@ -23,7 +23,7 @@ export const level6 = {
         { type: "bat", x: 400, y: 80 },   
    { type: "bat", x: 300, y: 80 },  
          { type: "clown", x: 400, y: 100 },
-       { type: "palm", x: 200, y: 120, width: 180, height: 100 },
+       { type: "palm", x: 200, y: 120, width: 240, height: 100 },
         { type: "exitGate", x: 700, y: 400, width: 64, height: 64 }
     ]
 };
