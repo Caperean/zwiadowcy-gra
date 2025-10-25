47538a34fcbb5cb9548af17f525b8dda59f40ef2
Caperean <sec.guild.deeeep@gmail.com>
2025-10-26 00:08:30 +0200

    Update level17.js

--- levels/level17.js
+++ levels/level17.js
@@ -2,24 +2,24 @@ export const level17 = {
     backgroundColor: "#add8e6",
     map: [
         ".........................R",                            // R-skała
-        ".........................R",                             // G-trawa
-        ".........................R",                             // #-blok ziemi
-        "K.....K......KKK......R",                             // N-śnieg
-        "K.....K......K.....K.....R",                            // I-lód
-        "K..KK.K......K.....K.....R",                            // L-lawa
-        "K.....K...KKKK.....K.....R",                           //  W-water 
-        "KK....K...K........K.....R",                          //   S-kolce
-        "K.KK..K...K.K......K.....R",                           //  M-magma
-        "K.....K...K....K...K.....R",                             // A-marmur
+        "................KKK......R",                             // G-trawa
+        "...........KKKKKK........R",                             // #-blok ziemi
+        "K.....K...KK.............R",                             // N-śnieg
+        "K.....K...K........K.....R",                            // I-lód
+        "K..KK.K...K..K..K..K.....R",                            // L-lawa
+        "K.....K...K........K.....R",                           //  W-water 
+        "KK....K...KK.......K.....R",                          //   S-kolce
+        "K.KK..K...K.K..KK..K.....R",                           //  M-magma
+        "K.....K...K........K.....R",                             // A-marmur
         "K....KK...K.......KK.....R",                           // C-cegły
         "K..KK.K...K......K.K.....R",                           
         "K.............K....K.....R",                           
-        "KK..........K............R",
-        "KKKKKKKRRRKSSSSS......PPPR",
+        "KK..........K......K.....R",
+        "KKKKKKKRRRKSSSSSSSSKSSPPPR",
     ],
     objects: [
-       { type: "player", x: 36, y: 390 },  
-        
-        { type: "exitGate", x: 25, y: 15, width: 64, height: 64 }
+       { type: "player", x: 360, y: 290 },  
+        { type: "arab", x: 70, y: 390 },
+        { type: "exitGate", x: 750, y: 350, width: 64, height: 64 }
     ]
 };
