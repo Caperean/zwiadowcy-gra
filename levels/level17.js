b806e2f5dd5a6bc28d5f84694157e58df0f75386
Caperean <sec.guild.deeeep@gmail.com>
2025-10-26 00:23:21 +0200

    Update level17.js

--- levels/level17.js
+++ levels/level17.js
@@ -2,7 +2,7 @@ export const level17 = {
     backgroundColor: "#add8e6",
     map: [
         "K................KKK.....R",                            // R-skała
-        "K..........K....K........R",                             // G-trawa
+        "K...............K........R",                             // G-trawa
         "K..........KKKKK.........R",                             // #-blok ziemi
         "K.....K...K..............R",                             // N-śnieg
         "K.....K...K........K.....R",                            // I-lód
